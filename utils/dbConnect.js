8bc2d75de4806786dce4b4a9a1c31882e6462b3c
ddoljugy <ddoljugy@gmail.com>
2024-10-21 18:27:10 +0900

    Update dbConnect.js

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,19 +1,35 @@
 const mysql = require("mysql2/promise");
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const dbConnect = async (region) => {
+  let connectionConfig;
 
-async function dbConnect() {
-  try {
-    await connection.getConnection();
-    console.log("MySQL connection established");
-  } catch (error) {
-    console.error("Database connection error:", error);
+  if (region === "seoul") {
+    connectionConfig = {
+      host: process.env.MYSQL_HOST_SEOUL,
+      user: process.env.MYSQL_USER_SEOUL,
+      password: process.env.MYSQL_PASSWORD_SEOUL,
+      database: process.env.MYSQL_DATABASE_SEOUL,
+    };
+  } else if (region === "sao_paulo") {
+    connectionConfig = {
+      host: process.env.MYSQL_HOST_SAO_PAULO,
+      user: process.env.MYSQL_USER_SAO_PAULO,
+      password: process.env.MYSQL_PASSWORD_SAO_PAULO,
+      database: process.env.MYSQL_DATABASE_SAO_PAULO,
+    };
+  } else {
+    throw new Error("Invalid region specified.");
   }
-}
 
-module.exports = { dbConnect, connection };
+  try {
+    const connection = mysql.createPool(connectionConfig);
+    await connection.getConnection();
+    console.log(`${region} MySQL connection established`);
+    return connection;
+  } catch (error) {
+    console.error(`${region} Database connection error:`, error);
+    throw error;
+  }
+};
+
+module.exports = { dbConnect };
